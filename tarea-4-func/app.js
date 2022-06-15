63efb3fa3d82da3e6341b8ec98a0cadf5a4d77a7
francisco-danelon <frandane.eth@gmail.com>
2022-06-15 09:37:57 -0300

    actualizo programa

--- tarea-4-func/app.js
+++ tarea-4-func/app.js
@@ -1,12 +1,24 @@
 const productos = [
-    {nombre: "Rig", precio: 10000},
-    {nombre: "Curso", precio: 1500},
+    {nombre: "Rig", precio: 9000},
+    {nombre: "Rig 3080ti", precio: 12000},
+    {nombre: "Curso Blockchain", precio: 1500},
+    {nombre: "Curso Solidity", precio: 1800},
     {nombre: "Mantenimiento", precio: 800},
+    {nombre: "Mantenimiento quincenal", precio: 800},
     {nombre: "Housing", precio: 2000},
 ];
 
-// const nombres = productos.map(elemento => elemento.nombre);
-// console.log(nombres);
-let busqueda = prompt("Ingrese el producto que desea")
+let busqueda = prompt("Busque el producto que desea");
+
 const result = productos.filter(elemento => elemento.nombre.includes(busqueda));
 console.log(result);
+
+
+
+
+
+// const nombres = productos.map(elemento => elemento.nombre);
+// console.log(nombres);
+// let busqueda = prompt("Ingrese el producto que desea")
+// const result = productos.filter(elemento => elemento.nombre.includes(busqueda));
+// console.log(result);
